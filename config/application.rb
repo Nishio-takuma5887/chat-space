05907180e168a47cf3bf4cd8f8c8529ac2b330c1
Nishio-takuma5887 <shotterssters5778@gmail.com>
2020-09-18 16:41:28 +0900

    ユーザー設定

--- config/application.rb
+++ config/application.rb
@@ -17,10 +17,5 @@ module ChatSpace
       g.test_framework false
       config.i18n.default_locale = :ja
     end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
